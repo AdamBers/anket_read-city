a784ea3cbc6dbf6c35a7a059398f7a37899e1d90
adam <adambers2020@gmail.com>
2022-11-23 10:19:31 +0300

    added range condition

--- js/script.js
+++ js/script.js
@@ -10,10 +10,33 @@
 // }
 
 
-window.onclick = e => {
-    if (e.target.style.backgroundColor === "#fff" ) {
-        e.target.style.backgroundColor = "#2283C7";
-        e.target.style.color = "#fff";
-        console.log(e.target.className)
+// window.onclick = e => {
+//     if (e.target.style.backgroundColor === "#fff" ) {
+//         e.target.style.backgroundColor = "#2283C7";
+//         e.target.style.color = "#fff";
+//         console.log(e.target.className)
+//     }
+// } 
+
+
+
+
+// var rangeInput = document.getElementById("rs-range-line");
+
+// rangeInput.addEventListener('mouseup', function () {
+//     if (this.value > 0 && this.value < 5) {
+//         alert("First");
+//     } else {
+//         alert("Second");
+//     }
+// });
+
+
+function checkRange(event) {
+    if (event.target.value >= 6) {
+        document.getElementById("fourth-question").style.display = "none";
     }
-} 
+    if (event.target.value <= 5) {
+        document.getElementById("fourth-question").style.display = "block";
+    }
+}
