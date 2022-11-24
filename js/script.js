81665bc419e7a1c970396ca21cabadeb38b016d3
adam <adambers2020@gmail.com>
2022-11-24 04:34:07 +0300

    changed sub_answers

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
 window.onclick = event => {
     const backgroundColor = event.target.style.backgroundColor;
+
+
     if (backgroundColor === '' && event.target.classList.contains("answer")) {
         event.target.style.backgroundColor = '#2283C7';
         event.target.style.color = '#fff';
         event.target.style.borderColor = '#fff';
 
-    } else {
+    }
+    if (backgroundColor !== '' && event.target.classList.contains("answer")) {
         event.target.style.backgroundColor = '';
         event.target.style.color = '#2283C7';
-        event.target.style.borderColor = '#fff';
+        event.target.style.borderColor = '#2283C7';
+    }
+
+
+
+    if (backgroundColor !== '' && event.target.classList.contains("sub_answer")) {
+        event.target.style.backgroundColor = 'rgb(34, 131, 199)';
+        event.target.style.color = '';
 
     }
+    if ((backgroundColor === '' || backgroundColor === 'rgb(34, 131, 199)') && event.target.classList.contains("sub_answer")) {
+        event.target.style.backgroundColor = '#fff';
+        event.target.style.color = '#2283C7';
+
+    }
+
+
 }
 
 
