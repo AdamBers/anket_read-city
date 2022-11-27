6790760af9fdb07022bddd04cb8e29c104609534
adam <adambers2020@gmail.com>
2022-11-27 04:04:38 +0300

    added 4 questions

--- js/script.js
+++ js/script.js
@@ -1,86 +1,12 @@
-window.onclick = event => {
 
-
-    /////////////////////BG color change
-    const backgroundColor = event.target.style.backgroundColor;
-    if (backgroundColor === '' && event.target.classList.contains("answer")) {
-        event.target.style.backgroundColor = '#2283C7';
-        event.target.style.color = '#fff';
-        event.target.style.borderColor = '#fff';
-
-    }
-    if (backgroundColor !== '' && event.target.classList.contains("answer")) {
-        event.target.style.backgroundColor = '';
-        event.target.style.color = '#2283C7';
-        event.target.style.borderColor = '#2283C7';
-    }
-
-
-
-    if (backgroundColor !== '' && event.target.classList.contains("sub_answer")) {
-        event.target.style.backgroundColor = 'rgb(34, 131, 199)';
-        event.target.style.color = '';
-
-    }
-    if ((backgroundColor === '' || backgroundColor === 'rgb(34, 131, 199)') && event.target.classList.contains("sub_answer")) {
-        event.target.style.backgroundColor = '#fff';
-        event.target.style.color = '#2283C7';
-
-    }
-
-    ////////////////////////Show hide elements
-
-
-    if (event.target.parentElement.className === 'container-answers-4') {
-        event.target.parentElement.className = 'container-answers-4-closed'
-        console.log(event.target.parentElement.className)
-        // event.target.firstChild.style.backgroundColor = "red"
-    } else if (event.target.parentElement.className === 'container-answers-4-closed') {
-        event.target.parentElement.className = 'container-answers-4'
-        // event.target.firstChild.style.backgroundColor = "red"
-        console.log(event.target.parentElement.className)
-    }
-
-
-}
 
 
 function checkRange(event) {
+    var fourthQuestion = document.getElementById("fourth-question")
     if (event.target.value >= 6) {
-        document.getElementById("fourth-question").style.display = "none";
+        fourthQuestion.style.display = "block";
     }
     if (event.target.value <= 5) {
-        document.getElementById("fourth-question").style.display = "block";
+        fourthQuestion.style.display = "none";
     }
-}
-
-
-function showHideChild(event) {
-    // event.target.nextSibling.style.display = "none"
-    // event.target.nextElementSibling.style.display = "none"
-    if (event.srcElement.nextElementSibling.style.display === "none") {
-        event.srcElement.nextElementSibling.style.display = "block"
-        event.target.parentElement.style.backgroundColor = "#fff"
-    } else {
-        event.srcElement.nextElementSibling.style.display = "none"
-    }
-
-
-
-}
-
-
-
-
-function showHideElement(event) {
-    console.log(event.target)
-    var subGroup = document.getElementById("sub_group_price")
-    subGroup.classList.toggle("hidden")
-
-    
-    if (event.target.id === "hiPrices") {
-        var sub_answer_price = document.getElementById("sub_answer_price")
-        sub_answer_price.style.className = "sub_answer_price_opened"
-    }
-
 }
